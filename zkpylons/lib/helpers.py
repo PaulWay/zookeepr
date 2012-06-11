36462c405564bca59b833b837236eb3764958622
Paul Wayper <paulway@mabula.net>
2012-06-11 13:08:14 +1000

    Changing parameter 'set' (Python keyword) to 'fileset' in slideshow().

--- zkpylons/lib/helpers.py
+++ zkpylons/lib/helpers.py
@@ -177,7 +177,7 @@ rot_26 = "rot_13" #used for being sneaky in the tag hashing for LCA2012
 #        return "no images found"
 #
 
-def slideshow(set, small=None):
+def slideshow(fileset, small=None):
     """
     Generate a slideshow of a set of images, randomly selecting one to
     show first, unless a file is specified.
@@ -186,16 +186,16 @@ def slideshow(set, small=None):
         if small == None or small == "":
             # Randomly select a smaller image, set the width of the div to be
             # the width of image.
-            small = random.choice(glob(file_paths["public_path"] + "/images/" + set + "/small/*"))
+            small = random.choice(glob(file_paths["public_path"] + "/images/" + fileset + "/small/*"))
         else:
-            small = file_paths["public_path"] + "/images/" + set + "/small/" + small
+            small = file_paths["public_path"] + "/images/" + fileset + "/small/" + small
 
-        output = "<div class=\"slideshow\" id=\"%s\" style=\"width: %dpx\">" % (set, int(Image.open(small).size[0]))
+        output = "<div class=\"slideshow\" id=\"%s\" style=\"width: %dpx\">" % (fileset, int(Image.open(small).size[0]))
         small = os.path.basename(small)
 
         # Optionally load up some captions for the images.
         caption = dict()
-        caption_file = file_paths['public_path'] + "/images/" + set + "/captions"
+        caption_file = file_paths['public_path'] + "/images/" + fileset + "/captions"
         if os.path.exists(caption_file):
             file = open(caption_file, 'r')
             captions = file.readlines()
@@ -205,15 +205,15 @@ def slideshow(set, small=None):
                 str = cap.partition(':')
                 caption[str[0]] = str[2]
 
-        # Load up all the images in the set directory.
-        files = glob(file_paths['public_path'] + "/images/" + set + '/*')
+        # Load up all the images in the fileset directory.
+        files = glob(file_paths['public_path'] + "/images/" + fileset + '/*')
         for file in files:
             if os.path.isfile(file):
                 short_file = os.path.basename(file)
                 if short_file == 'captions':
                     continue
 
-                output += "<a href=\"" + file_paths["public_html"] + "/images/" + set + "/" + short_file + "\" rel=\"lightbox[" + set + "]\""
+                output += "<a href=\"" + file_paths["public_html"] + "/images/" + fileset + "/" + short_file + "\" rel=\"lightbox[" + fileset + "]\""
                 if short_file in caption:
                     output += " title=\"" + caption[short_file] + "\""
                 output += ">"
@@ -221,7 +221,7 @@ def slideshow(set, small=None):
                 # If we're looking at the small one we've picked, display
                 # it as well.
                 if short_file == small:
-                    output += "<img src=\"" +  file_paths["public_html"] + "/images/" + set + "/small/" + short_file + "\">"
+                    output += "<img src=\"" +  file_paths["public_html"] + "/images/" + fileset + "/small/" + short_file + "\">"
 
                     # If there are more than one image in the slideshow
                     # then also display "more...".
